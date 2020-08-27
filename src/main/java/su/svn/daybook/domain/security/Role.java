f417e70210644ae6c7f96f7a2eceadf8118cb6b9
Victor N. Skurikhin <VNSkurikhin@sberbank.ru>
2020-08-27 08:28:20 +0300

    2. Model use hasRole at @PreAuthorize (at section 7. http API in this article), by default you have to add ROLE_ prefix. See https://docs.spring.io/autorepo/docs/spring-security/5.3.1.RELEASE/reference/html5/#el-access for more info.

---
+++ src/main/java/su/svn/daybook/domain/security/Role.java
@@ -0,0 +1,13 @@
+/*
+ * This file was last modified at 2020.02.10 21:35 by Victor N. Skurikhin.
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org>
+ * Role.java
+ * $Id$
+ */
+
+package su.svn.daybook.domain.security;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
