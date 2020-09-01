2cad23f9e459a4f7d1d53f7449fe3695da169645
Victor N. Skurikhin <VNSkurikhin@sberbank.ru>
2020-09-01 16:17:13 +0300

    Done: PBKDF2EncoderTest.

--- src/test/java/su/svn/daybook/SpringBootUnitsTest.java
+++ src/test/java/su/svn/daybook/SpringBootUnitsTest.java
@@ -5,31 +5,62 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import su.svn.daybook.domain.security.TestDataSecurity;
 import su.svn.daybook.services.security.JWTUtil;
+import su.svn.daybook.services.security.PBKDF2Encoder;
 
 import java.util.Date;
 
 @SpringBootTest
 public class SpringBootUnitsTest {
 
-    public static final String TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjpbIlJPTEVfVVNFUiJdLCJzdWIiOiJ1c2VyIiwiaWF0Ijox" +
-            "NTk4OTYzOTg5LCJleHAiOjE1OTg5OTI3ODl9.z5rd_bvC7ZYFLfRBY6pr1jtMr7fRyQ-I0jj7GmRBl1sYd28caMrmxf1M1QTjyzV715o" +
-            "Lwj6q95jMUl2Y6uHvYQ";
+    public static final String TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VybmFtZTEiLCJpYXQiOjE1OTg5NjU2MTIsImV4cCI6" +
+            "MTU5ODk5NDQxMn0.GbhnhuEtscd4FymYDB0O1RAwFgxX7pnLfQLT9Jcrxa_TIZhsV2UhZjwkWDSw5Ygx-jNHrkqMOYd3OE2EApWzLA";
+
+    public static final String ENCODED = "cBrlgyL2GI2GINuLUUwgojITuIufFycpLG4490dhGtY=";
 
     @Autowired
     JWTUtil jwtUtil;
+
+    @Autowired
+    PBKDF2Encoder pbkdf2Encoder;
 
     @Nested
     class JWTUtilTest {
 
         @Test
         void getUsernameFromToken() {
-            Assertions.assertEquals("user", jwtUtil.getUsernameFromToken(TOKEN));
+            Assertions.assertEquals("username1", jwtUtil.getUsernameFromToken(TOKEN));
         }
 
         @Test
         void getExpirationDateFromToken() {
-            Assertions.assertEquals(new Date(1598992789000L), jwtUtil.getExpirationDateFromToken(TOKEN));
+            Assertions.assertEquals(new Date(1598994412000L), jwtUtil.getExpirationDateFromToken(TOKEN));
+        }
+
+        @Test
+        void generateToken() {
+            Assertions.assertNotNull(TOKEN, jwtUtil.generateToken(TestDataSecurity.USER_1));
+        }
+
+        @Test
+        void validateToken() {
+            Assertions.assertTrue(jwtUtil.validateToken(TOKEN));
+        }
+    }
+
+    @Nested
+    class PBKDF2EncoderTest {
+
+        @Test
+        void encode() {
+            Assertions.assertEquals(ENCODED, pbkdf2Encoder.encode("user"));
+        }
+
+        @Test
+        void matches() {
+            Assertions.assertTrue(pbkdf2Encoder.matches("user", ENCODED));
+            Assertions.assertFalse(pbkdf2Encoder.matches("abc", ENCODED));
         }
     }
 }
