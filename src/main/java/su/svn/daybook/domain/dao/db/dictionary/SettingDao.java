1221202cfcaf48450a61d2b59f1e8ab3e969d744
Victor N. Skurikhin <VNSkurikhin@sberbank.ru>
2021-01-16 15:51:24 +0300

    Add tables: dictionary.value_type and dictionary.setting. Some fix in security.role, dictionary.codifier, dictionary.word, dictionary.vocabulary, dictionary.tag_label, dictionary.language and dictionary.i18n.

---
+++ src/main/java/su/svn/daybook/domain/dao/db/dictionary/SettingDao.java
@@ -0,0 +1,15 @@
+/*
+ * This file was last modified at 2021.01.16 15:51 by Victor N. Skurikhin.
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org>
+ * SettingDao.java
+ * $Id$
+ */
+
+package su.svn.daybook.domain.dao.db.dictionary;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import su.svn.daybook.domain.model.db.dictionary.Setting;
+
+public interface SettingDao extends ReactiveCrudRepository<Setting, Long> {
+}
