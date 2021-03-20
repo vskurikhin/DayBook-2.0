09f69a50b323d4aa9a0694921d4ea1613b2e71b4
Victor N. Skurikhin <VNSkurikhin@sberbank.ru>
2021-03-20 15:52:13 +0300

    Add resourceRecord and resourceRecordActions. Refactoring EditNewsEntry.

--- src/main/js/reducers/index.js
+++ src/main/js/reducers/index.js
@@ -1,5 +1,5 @@
 /*
- * This file was last modified at 2021.03.02 23:08 by Victor N. Skurikhin.
+ * This file was last modified at 2021.03.20 15:52 by Victor N. Skurikhin.
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org>
  * index.js
@@ -9,7 +9,7 @@
 import currentDate from './currentDate';
 import currentUser from './currentUser';
 import language from './language';
-import updatedRecord from './updatedRecord';
+import resourceRecord from "./resourceRecord";
 
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
@@ -19,5 +19,5 @@ export default combineReducers({
   currentDate: currentDate,
   currentUser,
   language,
-  updatedRecord: updatedRecord,
+  resourceRecord,
 });
---
+++ src/main/js/redux/resourceRecordActions.js
@@ -0,0 +1,31 @@
+/*
+ * This file was last modified at 2021.03.20 15:52 by Victor N. Skurikhin.
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org>
+ * resourceRecordActions.js
+ * $Id$
+ */
+
+export const RESOURCE_RECORD_PENDING = 'RESOURCE_RECORD_PENDING';
+export const RESOURCE_RECORD_SUCCESS = 'RESOURCE_RECORD_SUCCESS';
+export const RESOURCE_RECORD_ERROR = 'RESOURCE_RECORD_ERROR';
+
+export function resourceRecordPending() {
+    return {
+        type: RESOURCE_RECORD_PENDING
+    }
+}
+
+export function resourceRecordSuccess(value) {
+    return {
+        type: RESOURCE_RECORD_SUCCESS,
+        resourceRecord: value
+    }
+}
+
+export function resourceRecordError(error) {
+    return {
+        type: RESOURCE_RECORD_ERROR,
+        error: error
+    }
+}
